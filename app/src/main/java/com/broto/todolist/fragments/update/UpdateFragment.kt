4e777826fd274c141f90e68cc42963536962bcfd
Anibrata Saha <broto76@gmail.com>
2021-05-23 11:52:12 +0530

    Added menu navigation fragments and animations to the actions between 2 navigation fragments

--- app/src/main/java/com/broto/todolist/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/broto/todolist/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.broto.todolist.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.broto.todolist.R
 
 class UpdateFragment : Fragment() {
@@ -14,8 +12,14 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
